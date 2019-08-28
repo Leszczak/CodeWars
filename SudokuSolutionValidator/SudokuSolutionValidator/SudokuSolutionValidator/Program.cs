680d97ea5bf2f425a897cde99d5e537063abc981
Pawel <leszczak.pm@gmail.com>
2019-08-28 16:05:20 +0200

    add solution

---
+++ SudokuSolutionValidator/SudokuSolutionValidator/SudokuSolutionValidator/Program.cs
@@ -0,0 +1,40 @@
+﻿public class Sudoku
+{
+    public static bool ValidateSolution(int[][] board)
+    {
+        //first dimensions check
+        for (int i = 0; i < 9; i++)
+        {
+            int sum = 0;
+            for (int j = 0; j < 9; j++)
+                sum += board[i][j];
+            if (sum != 45)
+                return false;
+        }
+        //second dimensions check
+        for (int i = 0; i < 9; i++)
+        {
+            int sum = 0;
+            for (int j = 0; j < 9; j++)
+                sum += board[j][i];
+            if (sum != 45)
+                return false;
+        }
+        //check 9 squares
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                int sum = 0;
+                for (int k = 0; k < 3; k++)
+                    for (int l = 0; l < 3; l++)
+                    {
+                        sum += board[k + 3 * i][l + 3 * j];
+                    }
+                if (sum != 45)
+                    return false;
+            }
+        }
+        return true;
+    }
+}
