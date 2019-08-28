aede9780b4ebf49d60e7f4f5aa2d77b9ac3d6c96
Pawel <leszczak.pm@gmail.com>
2019-08-28 12:50:07 +0200

    Add negative input support

--- MilionthFibonacci/MilionthFibonacci/MilionthFibonacci/Program.cs
+++ MilionthFibonacci/MilionthFibonacci/MilionthFibonacci/Program.cs
@@ -3,23 +3,36 @@ public class Fibonacci
 {
     public static BigInteger fib(int n)
     {
-        if (n == 0)
-            return 0;
         BigInteger a = 0;
         BigInteger b = 1;
-        if(n > 0)
+        if (n == 0)
+            return new BigInteger(0);
+        else if (n > 0)
         {
-            for(int i=0; i<n-1;i++)
+            for (int i = 0; i < n - 1; i++)
             {
-                //a is next fib number
                 a += b;
                 //swap numbers
                 a = a + b;
                 b = a - b;
                 a = a - b;
             }
+            return b;
         }
-        return b;
+        else if (n < 0)
+        {
+            for (int i = 0; i < -n; i++)
+            {
+                b -= a;
+                //swap numbers
+                a = a + b;
+                b = a - b;
+                a = a - b;
+            }
+            return a;
+        }
+        else //duh
+            return new BigInteger(0);
     }
 
     public static void Main(string[] args)
--- MilionthFibonacci/MilionthFibonacci/MilionthFibonacciTests/UnitTest1.cs
+++ MilionthFibonacci/MilionthFibonacci/MilionthFibonacciTests/UnitTest1.cs
@@ -28,5 +28,14 @@ namespace Tests
             Assert.AreEqual(new BigInteger(4181), Fibonacci.fib(19), "For 19 result should be 4181");
             Assert.AreEqual(new BigInteger(6765), Fibonacci.fib(20), "For 20 result should be 6765");
         }
+
+        [Test]
+        public void TestForNeg()
+        {
+            Assert.AreEqual(new BigInteger(1), Fibonacci.fib(-1), "For -1 result should be 1");
+            Assert.AreEqual(new BigInteger(-1), Fibonacci.fib(-2), "For -2 result should be -1");
+            Assert.AreEqual(new BigInteger(2), Fibonacci.fib(-3), "For -3 result should be 2");
+            Assert.AreEqual(new BigInteger(-3), Fibonacci.fib(-4), "For -4 result should be -3");
+        }
     }
 }
