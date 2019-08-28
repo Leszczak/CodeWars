55474b8aa26bfdf83a4c6af324aefe9ecaaadfcf
Pawel <leszczak.pm@gmail.com>
2019-08-28 12:36:36 +0200

    Add base program and tests

---
+++ MilionthFibonacci/MilionthFibonacci/MilionthFibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+public class Fibonacci
+{
+    public static BigInteger fib(int n)
+    {
+        if (n == 0)
+            return 0;
+        BigInteger a = 0;
+        BigInteger b = 1;
+        if(n > 0)
+        {
+            for(int i=0; i<n-1;i++)
+            {
+                //a is next fib number
+                a += b;
+                //swap numbers
+                a = a + b;
+                b = a - b;
+                a = a - b;
+            }
+        }
+        return b;
+    }
+
+    public static void Main(string[] args)
+    {
+
+    }
+}
---
+++ MilionthFibonacci/MilionthFibonacci/MilionthFibonacciTests/UnitTest1.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+using System.Numerics;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestForSmall()
+        {
+            Assert.AreEqual(new BigInteger(0), Fibonacci.fib(0), "For 0 result should be 0");
+            Assert.AreEqual(new BigInteger(1), Fibonacci.fib(1), "For 1 result should be 1");
+            Assert.AreEqual(new BigInteger(1), Fibonacci.fib(2), "For 2 result should be 1");
+            Assert.AreEqual(new BigInteger(2), Fibonacci.fib(3), "For 3 result should be 2");
+            Assert.AreEqual(new BigInteger(3), Fibonacci.fib(4), "For 4 result should be 3");
+        }
+
+        [Test]
+        public void TestForMid()
+        {
+            Assert.AreEqual(new BigInteger(2584), Fibonacci.fib(18), "For 18 result should be 2584");
+            Assert.AreEqual(new BigInteger(4181), Fibonacci.fib(19), "For 19 result should be 4181");
+            Assert.AreEqual(new BigInteger(6765), Fibonacci.fib(20), "For 20 result should be 6765");
+        }
+    }
+}
