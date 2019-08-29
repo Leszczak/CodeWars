ba8deefd57fde6b04e710e1f0256dcd77af4f141
Pawel <leszczak.pm@gmail.com>
2019-08-29 10:26:31 +0200

    Add solution

---
+++ StripComments/StripComments/StripComments/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+public class StripCommentsSolution
+{
+    public static string StripComments(string text, string[] commentSymbols)
+    {
+        return text.Split("\n")
+                    .Select(s => { foreach (string com in commentSymbols) s = s.Split(com)[0]; return s.TrimEnd(); })
+                    .Aggregate((a, b) => a + Environment.NewLine + b);
+    }
+}
